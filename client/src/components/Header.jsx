22d419fd3cd067531e177c9537c5ec750064b3aa
yike-qian-jin <ahmedyoussef_17@hotmail.fr>
2023-12-29 16:56:37 +0100

    fix searchHeader useEffect + search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,12 +17,12 @@ function Header() {
   };
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get("searchTerm");
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
-  }, []);
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
