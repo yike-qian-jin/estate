14a0204b2f7d6a57154b97a65efe3297ccc0b071
yike-qian-jin <ahmedyoussef_17@hotmail.fr>
2023-12-31 16:17:23 +0100

    rm dup + complete home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl cursor-pointer">
-            <span className="text-slate-500">Carasuma</span>
+            <span className="text-slate-500">C</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
